113aa73dc5885cde4ced45e3f7a083f1ed7efd5e
LaibaZeenat <laibazeenia42@gmail.com>
2026-04-13 16:17:02 +0500

    todo app

---
+++ script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById("Inputfield")
+let button = document.getElementById("btn")
+let list = document.getElementById("listContainer")
+
+        button.addEventListener('click',()=>{
+            let li = document.createElement("li");
+            li.innerHTML = input.value; 
+            list.appendChild(li);
+            input.value = ""
+            let span = document.createElement("span")
+            span.innerHTML = "\u00d7"
+            li.appendChild(span)
+            saveData();
+        })
+
+        list.addEventListener('click', function(e){
+            if(e.target.tagName === "LI"){
+                e.target.classList.toggle("checked");
+                saveData();
+            }
+            else if(e.target.tagName === "SPAN"){
+                e.target.parentElement.remove();
+                saveData();
+            }
+        },false)
+
+        function saveData(){
+            localStorage.setItem("data",list.innerHTML)
+        }
+        function showTask(){
+            list.innerHTML = localStorage.getItem("data");
+        }
+        showTask();
